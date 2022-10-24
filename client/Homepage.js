b8edd18a5b8d4986df20c918cbd4221cc2c237c1
Eric Stevens <uspeople@icloud.com>
2022-10-23 20:44:24 -0400

    Added pressing enter to also search

--- client/Homepage.js
+++ client/Homepage.js
@@ -1,3 +1,13 @@
+window.onload = function(){
+document.getElementById('searchbar').addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("searchbutton").click();
+  }
+});
+}
+
+
 function search_recipes() {
     testSearch = document.getElementById('searchbar').value
     testSearch=testSearch.toLowerCase();
