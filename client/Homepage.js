9a6870ee206fcb7ddecb5c5573c367738b2a5d01
Aryaan Bomma <aryaan18.bomma@gmail.com>
2022-10-25 19:32:49 -0400

    formatted json

--- client/Homepage.js
+++ client/Homepage.js
@@ -2,17 +2,31 @@ import * as crud from './crud.js';
 
 window.onload = async function () {
 
-
-
   if (document.URL.includes("SearchResults.html")) {
     let recipe = await crud.readRecipes();
+    recipe = recipe[0];
     let parent = document.getElementById("search")
-    // console.log(recipe)
-    let data = document.createElement("div");
+    let ingredients = recipe.ingredients;
+    let name = recipe.name;
+    let steps = recipe.steps;
     console.log(recipe)
-    data.innerHTML = JSON.stringify(recipe);
+    let l1 = document.createElement("li");
+    l1.innerHTML = `Recipe Name: ${name}`;
+    let l2 = document.createElement("li");
+    l2.innerHTML = `Instructions: ${steps[0]}`;
+    let data = document.createElement("ul");
+    data.appendChild(l1);
+    ingredients.forEach((ingredient, i) => {
+      let l = document.createElement("li");
+      if (i === 0) {
+        l.innerHTML = `Ingredients: ${ingredient}`;
+      } else {
+        l.innerHTML = ingredient;
+      }
+      data.appendChild(l);
+    });
+    data.appendChild(l2);
     parent.appendChild(data);
-
 
   } else {
     let searchbar = document.getElementById('searchbar');
@@ -28,5 +42,4 @@ window.onload = async function () {
       search_recipes();
     })
   }
-
 }
