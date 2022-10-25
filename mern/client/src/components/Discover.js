2768956c5479255a72940846213b32512a95b73e
RyeCai <70669000+RyeCai@users.noreply.github.com>
2022-10-25 19:59:12 -0400

    Fixing fetch calls

--- mern/client/src/components/Discover.js
+++ mern/client/src/components/Discover.js
@@ -1,34 +1,39 @@
 import React from "react";
 import Recipe from "./Recipe";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Discover() {
   const [recipes, setRecipes] = useState([]);
   useEffect(() => {
     async function getRecipes() {
-      const response = await fetch("/recipes");
-
+      const response = await fetch(`/recipes/`, { method: "GET" });
       if (!response.ok) {
-        const message = "GET request failed";
+        const message = `An error occured: ${response.statusText}`;
         window.alert(message);
         return;
       }
-
-      const recipes = await response.json();
-      console.log(recipes);
-
-      setRecipes(recipes);
+      const recipesList = await response.json();
+      console.log(recipesList);
+      setRecipes(recipesList);
     }
 
     getRecipes();
 
     return;
-  }, [recipes.length]);
-  return (
-    <div>
-      <Recipe recipe={recipes[0]}></Recipe>
-    </div>
-  );
+  }, []);
+  // async function getRecipes() {
+  //   try {
+  //     const response = await fetch(`/recipes`);
+  //     console.log(response);
+  //     const recipesList = response.json();
+  //     setRecipes(recipesList);
+  //   } catch (err) {
+  //     window.alert(err);
+  //   }
+  // }
+  // getRecipes();
+  return <div>{recipes[0] && <Recipe recipe={recipes[0]}></Recipe>}</div>;
 }
 
 export default Discover;
