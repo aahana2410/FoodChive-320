048a41d6f2c3a4e85ff16eb311ef754dd8185386
Aahana Sehrawat <as1053870@bose.com>
2022-11-07 12:18:36 -0500

    Added more options

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -99,6 +99,38 @@ function Search() {
       key: 'Fish'
     },
     {
+      cat: 'Ingredients',
+      key: 'Shellfish'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Pasta and Noodles'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Bread'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Broth'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Rice'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Other Seafood'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Other Poultry'
+    },
+    {
+      cat: 'Ingredients',
+      key: 'Other Red Meat'
+    },
+    {
       cat: 'Type',
       key: 'Breakfast'
     },
