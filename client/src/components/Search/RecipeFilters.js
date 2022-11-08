888792334decc05fe464bb9c02c12a338af44dfe
RiyanFarhat <59814090+RiyanFarhat@users.noreply.github.com>
2022-11-07 21:41:38 -0500

    Fixed some css

---
+++ client/src/components/Search/RecipeFilters.js
@@ -0,0 +1,148 @@
+const recipeFilters = [
+  {
+    cat: 'Cuisine',
+    key: 'American'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Chinese'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Japanese'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Korean'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Mexican'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Thai'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'French'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Italian'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Spanish'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Latin'
+  },
+  {
+    cat: 'Cuisine',
+    key: 'Mediterranean'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Chicken'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Pork'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Beef'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Fish'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Shellfish'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Pasta and Noodles'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Bread'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Broth'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Rice'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Other Seafood'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Other Poultry'
+  },
+  {
+    cat: 'Ingredients',
+    key: 'Other Red Meat'
+  },
+  {
+    cat: 'Type',
+    key: 'Breakfast'
+  },
+  {
+    cat: 'Type',
+    key: 'Lunch'
+  },
+  {
+    cat: 'Type',
+    key: 'Dinner'
+  },
+  {
+    cat: 'Type',
+    key: 'Dessert'
+  },
+  {
+    cat: 'Type',
+    key: 'Appetizer & Snack'
+  },
+  {
+    cat: 'Skill Level',
+    key: 'Beginner'
+  },
+  {
+    cat: 'Skill Level',
+    key: 'Intermediate'
+  },
+  {
+    cat: 'Skill Level',
+    key: 'Expert'
+  },
+  {
+    cat: 'Dietary Restrictions',
+    key: 'Vegetarian'
+  },
+  {
+    cat: 'Dietary Restrictions',
+    key: 'Vegan'
+  },
+  {
+    cat: 'Dietary Restrictions',
+    key: 'Gluten-Free'
+  },
+  {
+    cat: 'Dietary Restrictions',
+    key: 'Dairy Free'
+  },
+  {
+    cat: 'Dietary Restrictions',
+    key: 'Nut-Free'
+  }
+]
+
+export default recipeFilters
