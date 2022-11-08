5b5e53efec601af74bcf66d097fc425a4a72db62
RyeCai <70669000+RyeCai@users.noreply.github.com>
2022-11-08 10:03:35 -0500

    fixed origin url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://foodchive.onrender.com/",
+    origin: "https://foodchive.onrender.com",
   })
 );
 dotenv.config();
