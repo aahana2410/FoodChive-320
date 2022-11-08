22e65d4ef41fe366d6947af93eb4af51fe8056da
RyeCai <70669000+RyeCai@users.noreply.github.com>
2022-11-08 08:46:22 -0500

    add headers to responses

--- server/controllers/savedRecipeControllers.js
+++ server/controllers/savedRecipeControllers.js
@@ -1,38 +1,84 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 
-import SavedRecipe from '../models/savedRecipeModel.js';
+import SavedRecipe from "../models/savedRecipeModel.js";
 
 const router = express.Router();
 
-export const saveRecipe = async (req, res) => {
-  const { name, src, imgs, steps, ingredients, fltr_cuisine, fltr_ingredients, fltr_restrictions, fltr_skill, fltr_type } = req.body;
-  const newRecipe = new SavedRecipe({ name, src, imgs, steps, ingredients, fltr_cuisine, fltr_ingredients, fltr_restrictions, fltr_skill, fltr_type })
+export const saveRecipe = async (req, res, next) => {
+  const {
+    name,
+    src,
+    imgs,
+    steps,
+    ingredients,
+    fltr_cuisine,
+    fltr_ingredients,
+    fltr_restrictions,
+    fltr_skill,
+    fltr_type,
+  } = req.body;
+  const newRecipe = new SavedRecipe({
+    name,
+    src,
+    imgs,
+    steps,
+    ingredients,
+    fltr_cuisine,
+    fltr_ingredients,
+    fltr_restrictions,
+    fltr_skill,
+    fltr_type,
+  });
   try {
     await newRecipe.save();
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://foodchive.onrender.com/"
+    ); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.status(201).json(newRecipe);
+    next();
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
-}
+};
 
-export const getSavedRecipes = async (req, res) => {
+export const getSavedRecipes = async (req, res, next) => {
   try {
     const savedRecipes = await SavedRecipe.find();
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://foodchive.onrender.com/"
+    ); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.status(200).json(savedRecipes);
+    next();
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
-}
+};
 
-export const deleteSavedRecipe = async (req, res) => {
+export const deleteSavedRecipe = async (req, res, next) => {
   const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No saved recipe with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No saved recipe with id: ${id}`);
 
   await SavedRecipe.findByIdAndRemove(id);
-
+  res.header("Access-Control-Allow-Origin", "https://foodchive.onrender.com/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.json({ message: "Saved Recipe deleted successfully." });
-}
+  next();
+};
 
 export default router;
