eb9ccf108162e7b6be0d867d2ad3edfd8605aeca
RyeCai <70669000+RyeCai@users.noreply.github.com>
2022-11-14 00:59:03 -0500

    Styled the Navbar and added browser icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Discover from "./components/Discover/Discover";
 import Search from "./components/Search/Search";
 import Profile from "./components/Profile/Profile";
 import Saved from "./components/SavedRecipes/Saved";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-
 
 function App() {
   return (
@@ -18,10 +14,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="saved" element={<Saved />} />
-          <Route exact path="discover" element={<Discover />} />
-          <Route exact path="search" element={<Search />} />
-          <Route exact path="profile" element={<Profile />} />
+          <Route exact path="Saved" element={<Saved />} />
+          <Route exact path="Discover" element={<Discover />} />
+          <Route exact path="Search" element={<Search />} />
+          <Route exact path="Profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
